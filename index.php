363d91429758cc516f939f1cca3401aa7887515e
Michal-Placek <michal.placek@student.ossp.cz>
2020-02-18 08:54:47 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+//název proměnné
+$name = "Michal Plaček"
+$gender = "Muž"
+$school = "5. Základní škola Kolín"
+$email = "michal.placek@student.ossp.cz"
+$phone = "792306704"
+$hobbies = "Motocross, Stavění počítačů"
+$
+
+?>
+
+<!DOCTYPE html>
+<html lang="cs">
+ <head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Document</title>
+ </head>
+       <body>
+            <?php echo $name;?>
+
+            
+            
+                    
+               
+               <input type="submit" value="Odeslat"/>
+            </form>
+       </body>
+       
+</html>
